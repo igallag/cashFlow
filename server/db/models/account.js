fbd1d5026317b4561cf880f98f383bad0683b971
Ian Gallgher <igallag93@gmail.com>
2020-05-13 13:27:58 -0400

    added an account model for the DB

--- server/db/models/account.js
+++ server/db/models/account.js
@@ -1,4 +1,30 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Account = db.define('account', {})
+const Account = db.define('account', {
+  accountID: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  itemID: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  institutionID: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  insittutionName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  accountName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  accountType: {
+    type: Sequelize.STRING
+  }
+})
+
+export default Account
